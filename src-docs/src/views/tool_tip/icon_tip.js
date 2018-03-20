fbb68c3316fc43e3be606090ecc7a36bf0631d33
arkwright <1445684+arkwright@users.noreply.github.com>
2018-03-20 18:23:25 -0400

    Demo of EuiIconTip weirdness.

--- src-docs/src/views/tool_tip/icon_tip.js
+++ src-docs/src/views/tool_tip/icon_tip.js
@@ -14,14 +14,14 @@ export default () => (
       <EuiFlexItem grow={false}>
         <EuiCheckbox
           id="explainedCheckbox"
-          label="Use source maps"
+          label="Checkbox 1"
           onChange={() => {}}
         />
       </EuiFlexItem>
 
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem>
         <EuiIconTip
-          content="Source maps allow browser dev tools to map minified code to the original source code"
+          content="Popover in wrong location, grow=true"
           position="right"
         />
       </EuiFlexItem>
@@ -29,10 +29,32 @@ export default () => (
 
     <EuiSpacer />
 
-    <EuiIconTip
-      aria-label="Warning"
-      type="alert"
-      content="I do not think it means what you think it means"
-    />
+    <EuiFlexGroup alignItems="center" gutterSize="s">
+      <EuiFlexItem grow={false}>
+        <EuiCheckbox
+          id="explainedCheckbox"
+          label="Checkbox 2"
+          onChange={() => {}}
+        />
+      </EuiFlexItem>
+
+      <EuiFlexItem grow={false}>
+        <EuiIconTip
+          content="This EuiFlexItem has grow=false and I'm forcing it wider with some text."
+          position="right"
+        />
+        This EuiFlexItem has grow=false and I'm forcing it wider with some text.
+      </EuiFlexItem>
+    </EuiFlexGroup>
+
+    <EuiSpacer />
+
+    <div style={{ width: 200 }}>
+      <EuiIconTip
+        aria-label="Warning"
+        type="alert"
+        content="A wide div does not produce the same problem"
+      />
+    </div>
   </Fragment>
 );
